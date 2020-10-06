baecf0f81c417227debcc177c421e502488df0f2
Chai Nunes <chaiyawahoo@gmail.com>
2020-10-06 11:27:44 -0700

    update

---
+++ script.js
@@ -0,0 +1,110 @@
+const display = document.getElementById("display");
+
+
+// ========== Font Size ==========
+const inputSize = document.getElementById("input-size");
+const showSize = document.getElementById("show-size");
+function changeSize() {
+    const fontSize = inputSize.value;
+    display.style.fontSize = `${fontSize}px`;
+    showSize.innerText = fontSize;
+}
+inputSize.addEventListener("input", changeSize);
+changeSize();
+
+// ========== Letter Spacing ==========
+const inputSpacing = document.getElementById("input-spacing");
+const showSpacing = document.getElementById("show-spacing");
+function changeSpacing() {
+    const letterSpacing = inputSpacing.value;
+    display.style.letterSpacing = `${letterSpacing}px`;
+    showSpacing.innerText = letterSpacing;
+}
+inputSpacing.addEventListener("input", changeSpacing);
+changeSpacing();
+
+// ========== Padding ==========
+const inputPadding = [
+        document.getElementById("input-padding-1"), 
+        document.getElementById("input-padding-2"), 
+        document.getElementById("input-padding-3"), 
+        document.getElementById("input-padding-4")
+    ];
+const showPadding = document.getElementById("show-padding");
+function changePadding() {
+    const paddingValues = []
+    for (let i = 0; i < inputPadding.length; i++) {
+        paddingValues[i] = inputPadding[i].value;
+    }
+    const padding = `${paddingValues[0]}px ${paddingValues[1]}px ${paddingValues[2]}px ${paddingValues[3]}px`;
+    display.style.padding = padding;
+    showPadding.innerText = padding;
+}
+for (let i = 0; i < inputPadding.length; i++) {
+    inputPadding[i].addEventListener("input", changePadding);
+}
+changePadding();
+
+// ========== Font Family ==========
+const selectFont = document.getElementById("select-font");
+const showFont = document.getElementById("show-font");
+function changeFont() {
+    const fontFamily = selectFont.value;
+    display.style.fontFamily = fontFamily;
+    showFont.innerText = fontFamily;
+}
+selectFont.addEventListener("change", changeFont);
+changeFont();
+
+// ========== Font Weight ==========
+const selectWeight = document.getElementById("select-weight");
+const showWeight = document.getElementById("show-weight");
+function changeWeight() {
+    const fontWeight = selectWeight.value;
+    display.style.fontWeight = fontWeight;
+    showWeight.innerText = fontWeight;
+}
+selectWeight.addEventListener("change", changeWeight);
+changeWeight();
+
+// ========== Font Style ==========
+const selectStyle = document.getElementById("select-style");
+const showStyle = document.getElementById("show-style");
+function changeStyle() {
+    const fontStyle = selectStyle.value;
+    display.style.fontStyle = fontStyle;
+    showWeight.innerText = fontStyle;
+}
+selectStyle.addEventListener("change", changeStyle);
+changeStyle();
+
+// ========== Color ==========
+const inputColor = document.getElementById("input-color");
+const showColor = document.getElementById("show-color");
+function changeColor() {
+    const color = inputColor.value;
+    display.style.color = color;
+    showColor.innerText = color;
+}
+inputColor.addEventListener("input", changeColor);
+changeColor();
+
+// ========== Background Color ==========
+const inputBgColor = document.getElementById("input-bg-color");
+const showBgColor = document.getElementById("show-bg-color");
+function changeBgColor() {
+    const bgColor = inputBgColor.value;
+    display.style.backgroundColor = bgColor;
+    showBgColor.innerText = bgColor;
+}
+inputBgColor.addEventListener("input", changeBgColor);
+changeBgColor();
+
+// ========== Text ==========
+const enterText = document.getElementById("enter-text");
+function changeText() {
+    const text = enterText.value;
+    display.innerText = text;
+}
+enterText.addEventListener("input", changeText);
+changeText();
